6497557cf0d80a5d0c67737daa2fe31671511a15
WeinbergSolution <pascalgiercke@gmail.com>
2026-07-10 03:44:34 +0200

    fix: align raid calculator with workbench monitor

--- apps/web/src/features/tools/raid-calculator/RaidCalculatorWorkbenchFrame.tsx
+++ apps/web/src/features/tools/raid-calculator/RaidCalculatorWorkbenchFrame.tsx
@@ -2,13 +2,11 @@
 import './RaidCalculatorWorkbenchFrame.css';
 import { RaidCalculator } from './RaidCalculator';
 
-import workbenchImg from '../../../assets/Gemini_Generated_Image_4ana8h4ana8h4ana.png';
-
 export function RaidCalculatorWorkbenchFrame() {
   return (
-    <div className="workbench-frame-container" style={{ backgroundImage: `url(${workbenchImg})` }}>
-      <div className="workbench-monitor-bezel">
-        <div className="workbench-screen">
+    <div className="workbench-frame-container">
+      <div className="workbench-image-wrapper">
+        <div className="workbench-screen-overlay">
           <div className="screen-scanlines"></div>
           <div className="screen-content">
             <RaidCalculator />
